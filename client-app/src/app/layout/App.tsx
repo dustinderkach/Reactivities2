606610d731ac730bdad4bc42bece4e6f67c205c9
dustinderkach <dustinderkach2@gmail.com>
2021-02-15 21:41:54 -0600

    NewCourseCode_EndOfSection8

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+
+import { Container } from 'semantic-ui-react';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { observer } from 'mobx-react-lite';
+import { Route, useLocation } from 'react-router-dom';
+import HomePage from '../../features/activities/Home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+
+function App() {
+
+  const location = useLocation();
+
+  return (
+    <>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        //any route that matches the forward slash plus somenthing else will match the route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+      />
+
+
+    </>
+  );
+}
+
+export default observer(App);
